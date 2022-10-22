c0e981ddabeadd2a54af48347107015e37d9ed48
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-10-22 16:59:48 +0800

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,39 @@
+#tipe data integer
+
+panjang = 10
+
+print(panjang)
+
+#tipe data float
+
+lebar = 5.5
+
+print(lebar)
+
+#tipe data complex
+
+a = 5j
+
+b = 10j
+
+c = a + b
+
+print(a, '+', b, '=', c)
+
+#tipe data string
+
+data = "DQLab 2021"
+
+print(data)
+
+#penjumlahan dua data numerik
+
+print(2+2)
+
+Output: 4
+
+#penjumlahan dua data string
+
+print("2" + "2")
+
+Output: 22
